85c55702c034b749b8b711600131f7008f014085
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-12 00:20:42 -0400

    feat: vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,13 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('GEO_LOCATION_API_KEY') { ENV['GEO_LOCATION_API_KEY'] }
+  config.filter_sensitive_data('WEATHER_API_KEY') { ENV['WEATHER_API_KEY'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.default_cassette_options = { allow_playback_repeats: true }
+  config.configure_rspec_metadata!
+end
