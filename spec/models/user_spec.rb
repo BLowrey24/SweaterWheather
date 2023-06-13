720a67a32bc0ae19189220f9d8a27200587c324d
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-12 22:45:13 -0400

    test: for instance method generate_api_key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe User do
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:password_confirmation) }
   end
+
+  describe 'instance methods' do
+    describe '#generate_api_key' do
+      let(:user) { User.create!(email: "baston@example.com", password: "password", password_confirmation: "password") }
+      it 'generates an api key on creation' do
+        expect(user.api_key).to be_present
+      end
+    end
+  end
 end
