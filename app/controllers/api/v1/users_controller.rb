2890fd74bab49306e6059064e1b28a3d63c4a5ee
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-12 22:26:54 -0400

    feat: add functionality in users_controller and make user_serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    render json: UserSerializer.creation(user), status: 201
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
