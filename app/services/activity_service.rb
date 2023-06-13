814797ac02c4fa07f46394a2d7e156b0f464ee3f
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-13 13:45:39 -0400

    feat: add three more methods

--- app/services/activity_service.rb
+++ app/services/activity_service.rb
@@ -3,6 +3,18 @@ class  ActivityService
     get_url("/api/activity?destination=#{location}")
   end
 
+  def get_recreational_activity
+    get_url("/api/activity?type=recreational")
+  end
+
+  def get_busywork_activity
+    get_url("/api/activity?type=busywork")
+  end
+
+  def get_cooking_activity
+    get_url("/api/activity?type=cooking")
+  end
+
   private
     def conn 
       Faraday.new(url: 'https://www.boredapi.com')
