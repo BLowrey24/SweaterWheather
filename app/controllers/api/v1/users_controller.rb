f15e419817de4df6f81392257a3b70dc3506186c
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-12 23:48:37 -0400

    feat: updated users_controller to check different params and check handle sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-    if user.save
+    if params[:password] == params[:password_confirmation]
+      user = User.create!(user_params)
       render json: UserSerializer.creation(user), status: 201
+    else
+      render json: ErrorSerializer.serialize("Your passwords do not match"), status: 400
     end
   end
 
   private 
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
