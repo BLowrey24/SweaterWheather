aab2b55c713792bfc7f0b8c331940d50921aee11
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-12 23:47:19 -0400

    update: user_spec and user_create_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe User do
   describe 'validations' do
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:password_confirmation) }
   end
 
   describe 'instance methods' do
--- spec/requests/user/create_user_request_spec.rb
+++ spec/requests/user/create_user_request_spec.rb
@@ -1,36 +1,83 @@
 require 'rails_helper'
 
-RSpec.describe 'User Creation' do
-  describe 'happy path' do
-    it 'after user creation it returns their api key' do
-      post '/api/v1/users', params: { user: { email: "baston@example.com", password: "password", password_confirmation: "password" } }
+RSpec.describe 'Users API' do
+  before(:each) do
+    @headers = {"CONTENT_TYPE" => "application/json"}
+  end
+  describe 'POST /api/v1/users' do
+    it 'can create a new user' do
+      user = {
+        email: "baston@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+      post "/api/v1/users", headers: @headers, params: JSON.generate(user)
       expect(response).to be_successful
       expect(response.status).to eq(201)
-      body = JSON.parse(response.body, symbolize_names: true)
 
-      expect(body).to be_a Hash
-      expect(body[:data]).to be_a Hash
-      expect(body[:data][:type]).to eq("users")
-      expect(body[:data][:id]).to be_a String
-      expect(body[:data][:attributes]).to be_a Hash
-      expect(body[:data][:attributes][:email]).to eq("baston@example.com")
-      expect(body[:data][:attributes][:api_key]).to be_a String
+      user_data = JSON.parse(response.body, symbolize_names: true)
+      expect(user_data).to be_a Hash
+      expect(user_data).to have_key :data
+      expect(user_data[:data]).to be_a Hash
+      expect(user_data[:data]).to have_key :id
+      expect(user_data[:data][:id]).to be_a String
+      expect(user_data[:data]).to have_key :type
+      expect(user_data[:data][:type]).to eq('users')
+      expect(user_data[:data]).to have_key :attributes
+      expect(user_data[:data][:attributes]).to be_a Hash
+      expect(user_data[:data][:attributes]).to have_key :email
+      expect(user_data[:data][:attributes][:email]).to be_a String
+      expect(user_data[:data][:attributes]).to have_key :api_key
+      expect(user_data[:data][:attributes][:api_key]).to be_a String
+      expect(user_data[:data][:attributes]).to_not have_key :password_digest
     end
-  end
 
-  describe 'sad path' do
-    it 'returns an error if email is missing' do
-      post '/api/v1/users', params: { user: { password: "password", password_confirmation: "password" } }
+    it 'returns an error if email is already taken' do
+      User.create!(email: "baston@example.com", password: "password", api_key: "lfdkgj34t98sd34dfhj")
+      user_email_taken = {
+        email: "baston@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+      post "/api/v1/users", headers: @headers, params: JSON.generate(user_email_taken)
       expect(response).to_not be_successful
-      expect(response.status).to eq(400)
+      expect(response.status).to eq(409)
+
       body = JSON.parse(response.body, symbolize_names: true)
-      
       expect(body).to be_a Hash
       expect(body).to have_key :errors
       expect(body[:errors]).to be_a Array
       expect(body[:errors][0]).to be_a Hash
       expect(body[:errors][0]).to have_key :detail
-      expect(body[:errors][0][:detail]).to eq("email or password are invalid or missing")
+      expect(body[:errors][0][:detail]).to eq("Validation failed: Email has already been taken")
+    end
+
+    it 'returns an error if password and confirmation_password do not match' do
+      user_passwords_dont_match = {
+        email: "baston@example.com",
+        password: "password",
+        password_confirmation: "notmypassword"
+      }
+      post "/api/v1/users", headers: @headers, params: JSON.generate(user_passwords_dont_match)
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      expect(body[:errors][0][:detail]).to eq("Your passwords do not match")
+    end
+
+    it 'returns an error if email is missing' do
+      user_no_email = {
+        email: "",
+        password: "password",
+        password_confirmation: "password"
+      }
+      post "/api/v1/users", headers: @headers, params: JSON.generate(user_no_email)
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      expect(body[:errors][0][:detail]).to eq("Validation failed: Email can't be blank")
     end
   end
 end
