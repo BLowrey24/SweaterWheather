3ef58e0bdc93f1a16d48179971381d381f0172f9
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-12 23:47:56 -0400

    updated: user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 
   has_secure_password
   before_create :generate_api_key
-  
+
   private 
   
   def generate_api_key
