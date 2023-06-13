ec5821b6f4e5a981ce1339329d3139089d6d6a44
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-13 16:18:50 -0400

    update: user_serializer to be formatted right

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,8 @@ class UserSerializer
   def self.creation(user)
     {
       data: {
-        type: "users",
         id: "#{user.id}",
+        type: "users",
         attributes: {
           email: "#{user.email}",
           api_key: "#{user.api_key}"
