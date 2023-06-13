1c4bc4cfcb6efc7ff91d229f710ec4ffe4dffe03
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-13 17:07:00 -0400

    update: remove pry

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    # require 'pry'; binding.pry
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       render json: UserSerializer.creation(user), status: 200
