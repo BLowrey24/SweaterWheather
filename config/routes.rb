621961fc6102c2e4efaf88d320a1f174ab22cb00
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-13 12:22:23 -0400

    feat: add route and update service spec

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: [:index]
       resources :users, only: [:create]
+      rescources :activities, only: [:index]
     end
   end
 end
--- spec/services/activity_service_spec.rb
+++ spec/services/activity_service_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-#  require 'pry'; binding.pry
 RSpec.describe ActivityService do 
   describe 'Establish connection' do
-    it 'can connect to the Open Library API', :vcr => { :record => :new_episodes } do
+    it 'can connect to the Bored API', :vcr => { :record => :new_episodes } do
       activity_search = ActivityService.new.get_activities('chicago,il')
       expect(activity_search).to be_a Hash
       expect(activity_search).to have_key :activity
@@ -14,7 +13,6 @@ RSpec.describe ActivityService do
       expect(activity_search[:participants]).to be_a Integer
       expect(activity_search).to have_key :link
       expect(activity_search[:link]).to be_a String
-     
     end
   end
 end
