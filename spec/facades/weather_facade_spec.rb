d40281a67e017b9b20603f7f42d22d973646195a
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-13 12:36:35 -0400

    add tests

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -36,5 +36,27 @@ RSpec.describe WeatherFacade do
         expect(weather_object.hourly_weather.first).to have_key :conditions
         expect(weather_object.hourly_weather.first).to have_key :icon
     end
+
+    it 'returns a book-search object for a given location, with some weather', :vcr => { :record => :new_episodes } do
+      activities_by_location = WeatherFacade.new('chicago,il').activities_by_location
+      
+      expect(activities_by_location).to be_a Books
+      expect(activities_by_location.destination).to eq('denver,co')
+      expect(activities_by_location.forecast).to be_a Hash
+      expect(activities_by_location.forecast).to have_key :summary
+      expect(activities_by_location.forecast).to have_key :temperature
+      expect(activities_by_location.total_books_found).to be_an Integer
+      expect(activities_by_location.books).to be_an Array
+      expect(activities_by_location.books.count).to eq(5)
+      expect(activities_by_location.books.first).to be_a Hash
+      expect(activities_by_location.books.first).to have_key :isbn
+      expect(activities_by_location.books.first[:isbn]).to be_an Array
+      expect(activities_by_location.books.first[:isbn].first).to be_a String
+      expect(activities_by_location.books.first).to have_key :title
+      expect(activities_by_location.books.first[:title]).to be_a String
+      expect(activities_by_location.books.first).to have_key :publisher
+      expect(activities_by_location.books.first[:publisher]).to be_an Array
+      expect(activities_by_location.books.first[:publisher].first).to be_a String
+    end
   end
 end
